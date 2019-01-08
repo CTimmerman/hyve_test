cfd4923cf0dc597af011fe56f367b010eaff5446
Cees Timmerman <c.timmerman2@gmail.com>
2019-01-08 16:54:22 +0100

    encode() works on b'aabaabaa', but not b'bananas'.

---
+++ decoder.py
@@ -0,0 +1,89 @@
+"""Hyve decoder/encoder
+2019-01-08 v1.0 by Cees Timmerman
+"""
+import io, os, sys
+
+def decode_pair(encoded_pair, result=None):
+    """Starting with an empty result bytestring, read the encoded pair sequence (with
+    length n) from the left (i = 0) to the right. For each pair read, if pi = 0, append
+    qi to the output stream. Otherwise, pi > 0. Read the last pi characters appended
+    to the result string and take the first (from the left) qi characters.
+    For example, the following sequence:
+
+    (0, 61), (1, 1), (0, 62), (3, 2), (3, 3)
+    would result in the bytestring 61 61 62 61 61 62 61 61 (represented here in
+    hexadecimal)
+    """
+    if result is None: result = []
+    p, q = encoded_pair
+    if p == 0:
+        result.append(q)
+    elif p > 0:
+        result.extend(result[-p:][:q])
+    
+    return result
+
+def encode(byte_str):
+    result = []
+    i = 0
+    while i < len(byte_str):
+        b = byte_str[i]
+        # New byte? Add it.
+        try: p = byte_str[:i].index(b)
+        except ValueError:
+            result.extend((0, b))
+            i += 1
+            continue
+        # Seen byte? Encode seen part.
+        length = 1
+        while length < i and i+length < len(byte_str) \
+            and byte_str[p:p+length] == byte_str[i:i+length]:
+                length += 1
+        result.extend((i-p, length))
+        i += length
+    return bytes(result)
+
+def encode_simple(byte_str):
+    result = []
+    for b in byte_str:
+        result.extend((0, b))
+    return bytes(result)
+
+def decode_byte_stream(incoming):
+    result = []
+    while True:
+        pair = incoming.read(2)
+        if len(pair) == 0: break
+        try:
+            result = decode_pair(pair, result)
+        except:
+            result += bytes([0x3F])  # b'?'
+    return bytes(result)
+
+def decode(byte_str):
+    return decode_byte_stream(io.BytesIO(byte_str))
+
+def main(incoming=sys.stdin):
+    # Print decoded incoming bytes
+    result = decode_byte_stream(incoming)
+    print(result)
+    # Now output to stderr the re-encoded result
+    if os.environ.get('USE_TRIVIAL_IMPLEMENTATION'):
+        encoded = encode_simple(result)
+    else:
+        encoded = encode(result)
+    print(encoded, file=sys.stderr)
+    print(decode_byte_stream(io.BytesIO(encoded)))
+
+def test():
+    data = [0x0, 0x61, 0x1, 0x1, 0x0, 0x62, 0x3, 0x2, 0x3, 0x3]  # b'aabaabaa'
+    data = [0x00, 0x20, 0x00, 0x2A, 0x02, 0x01, 0xFF]  # b' * ?'
+    
+    """
+    b' * ?'
+    b'\x00 \x00*\x00 \x00?'
+    """
+    incoming = io.BytesIO(bytes(data))
+    main(incoming)
+
+if __name__ == "__main__": test()
